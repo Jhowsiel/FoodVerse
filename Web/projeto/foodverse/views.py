6d2fb358ad8742691e9a9c97efa4ad56c1d2c69c
Thiago <163765741+thbbsa@users.noreply.github.com>
2026-02-10 21:12:39 -0300

    web

---
+++ Web/projeto/foodverse/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Credenciais inválidas. Tente novamente.')
+    return render(request, 'pages/Autentificacao/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
